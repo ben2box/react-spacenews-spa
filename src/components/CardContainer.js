f53fc97a6ec9fc21a66dcbbf66a5345a0f6a0b0e
ben2box <pedrobento92@gmail.com>
2024-03-01 19:15:31 +0000

    Added basic pagination WIP

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -4,22 +4,28 @@ import Card from './Card'
 
 
 function CardContainer() {
-
+    
+    const [api, setApi] = useState('https://api.spaceflightnewsapi.net/v4/articles/?limit=6&offset=0')
     const [news, setNews] = useState([])
+    const [next, setNext] = useState('')
+    const [previous, setPrevious] = useState('')
+    const [page, setPage] = useState(1)
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(false)
 
     useEffect(() => {
       async function fetchNews() {
+        console.log(api)
         try {
           console.log('RUNNING USEEFFECT')  
           setLoading(true)
           console.log('FETCHING')  
-          const response = await fetch(`https://api.spaceflightnewsapi.net/v4/articles/`);
-          console.log(response)
+          const response = await fetch(`${api}`);
           const data = await response.json()
-          console.log(data)
+          setNext(data.next)
+          setPrevious(data.previous)
           setNews(data.results);
+          
           
         } catch (e) {
           setError(e);
@@ -30,27 +36,54 @@ function CardContainer() {
 
       }
       fetchNews()
-      
+    },[api, error]);
 
-    },[]);
-
-   
+    const handleOnClickNext = () => {
+      setApi(next)
+      setPage(p => p+1)
+    }
+    
+    const handleOnClickPrev = () => {
+      setApi(previous)
+      setPage(p => p-1)
+    }
 
     return (
-        <div id='cardContainer' className="row gy-2 gx-0 text-white justify-content-evenly">
-            {loading ? <h1> LOADING ... </h1> :
-            news.map((card) => (
-                <Card 
-                    id={card.id}
-                    title={card.title}
-                    url={card.url}
-                    image_url={card.image_url}
-                    summary={card.summary} />
-            ))}
-           
-            
+      <div
+        id="cardContainer"
+        className="row gy-2 gx-0 text-white justify-content-evenly"
+      >
+        <div id="btnContainer" className="text-center">
+          {previous !== null ? (
+            <>
+              <button className="btn btn-primary" onClick={handleOnClickPrev}>
+                Previous Page: {page-1}
+              </button>
+              <button className="btn btn-success" onClick={handleOnClickNext}>
+                Next Page: {page+1}
+              </button>
+            </>
+          ) : (
+            <button className="btn btn-success" onClick={handleOnClickNext}>
+              Next Page: {page+1}
+            </button>
+          )}
         </div>
-    )
+        {loading ? (
+          <h1> LOADING ... </h1>
+        ) : (
+          news.map((card) => (
+            <Card
+              id={card.id}
+              title={card.title}
+              url={card.url}
+              image_url={card.image_url}
+              summary={card.summary}
+            />
+          ))
+        )}
+      </div>
+    );
 }
 
 export default CardContainer;
