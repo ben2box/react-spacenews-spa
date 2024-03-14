f79d8b490d17ceebfe7964c57e3f81791b2466bf
ben2box <pedrobento92@gmail.com>
2024-03-14 15:42:15 +0000

    ready to deploy my mvp

--- src/components/notFound.jsx
+++ src/components/notFound.jsx
@@ -1,7 +1,12 @@
+import Divider from "./common/divider";
+
 export default function PageNotFound(){
     return (
-        <div className='loadingContainer container-fluid text-center justify-content-center'>
-            <h1> We couldn't find the page you were looking for...</h1>
-        </div>
+        <section>
+            <div className='loadingContainer container-fluid text-center justify-content-center'>
+                <h1> We couldn't find the page you were looking for...</h1>
+            </div>
+            <Divider/>
+        </section>
     )
 }
