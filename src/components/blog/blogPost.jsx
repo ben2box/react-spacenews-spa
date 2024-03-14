3934954ff52d67a38cf9519d9fb29631d1e9baca
ben2box <pedrobento92@gmail.com>
2024-03-14 11:36:59 +0000

    added some css hover animations

--- src/components/blog/blogPost.jsx
+++ src/components/blog/blogPost.jsx
@@ -1,20 +1,24 @@
+import { useState } from "react"
+
 export default function BlogPost({title, url, image_url, summary, publisher, date}) {
+    const [active, setActive] = useState(false)
+
     return (
         
-        <div className="mb-4 p-0 rounded" data-bs-theme='dark' style={{backgroundImage: `url(${image_url})`, "backgroundSize": "cover", "maxWidth":"80vw"}}>
-            <div className="darker-mask p-4 p-md-5 rounded" style={{"height":"50vh"}} >
-                <div className="col-10 col-lg-6 px-0">
+        <div className="blog-post-wrapper mb-4 p-0 rounded" onMouseEnter={() => setActive(true)} onMouseLeave={() => setActive(false)} data-bs-theme='dark' style={{backgroundImage: `url(${image_url})`, "backgroundSize": "cover", "maxWidth":"80vw"}}>
+                <a href={url} rel="noreferrer" target="_blank" style={{"display":"block", "height": "100%", "width":"100%", "textDecoration":"none"}}>
+            <div className="blog-post-container darker-mask p-4 p-md-5 rounded" style={{"minHeight":"50vh"}}>
+                <div className="col-10 col-xl-6 px-0">
                 <strong className="d-inline-block mb-2 text-info-emphasis">{publisher}</strong>
-                <h1 className="blog-title display-4 fst-italic">{title}</h1>
+                <h1 className="blog-title display-5 fst-italic">{title}</h1>
                 <div className="mb-1 text-body-secondary">{date.slice(0,10)}</div>
-                <p className="blog-lead lead my-4">{summary}</p>
-                <a href={url} rel="noreferrer" target="_blank">
-                    <button className="btn btn-outline-info fw-bold">
+                <p className="blog-lead lead">{summary}</p>
+                    <button className={`btn btn-outline-info fw-bold ${active? "active" : null}`}>
                         Read More
                     </button>
-                </a>
                 </div>
             </div>
+                </a>
         </div>
     
     )
--- src/components/news/newsCard.jsx
+++ src/components/news/newsCard.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
+
 function Card({title, url, image_url, summary}) {
+  const [active, setActive] = useState(false)
   return (
-      <div className="card col-auto">
-        <img className="img-fluid card-img-top" src={image_url} alt="News" />
+      <div className="card col-auto" onMouseEnter={() => setActive(true)} onMouseLeave={() => setActive(false)} >
+        <a href={url} target="_blank" rel="noreferrer"><img className="img-fluid card-img-top" src={image_url} alt="News" /></a>
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{summary}</p>
 
           <a href={url} rel="noreferrer" target="_blank">
-            <button className="btn btn-outline-info fw-bold">
+            <button className={`btn btn-outline-info fw-bold ${active? 'active' : null}`}>
               Read More
             </button>
           </a>
