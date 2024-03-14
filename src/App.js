7ad05edf23d988036afae7a472420c11ab5c08c5
ben2box <pedrobento92@gmail.com>
2024-03-14 13:43:56 +0000

    case typos

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 // import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
-import "./App.css";
+import "./app.css";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from "./components/header.jsx";
