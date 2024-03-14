ccb5e22c5da830a1e997e9ae11cd19f9c237272e
ben2box <pedrobento92@gmail.com>
2024-03-14 15:35:54 +0000

    changed basepath

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ function App() {
     <div className="App">
       <DarkModeProvider>
         <SearchTermProvider>
-          <Router>
+          <Router basepath="/react-spacenews-spa">
             <Header />
             <Routes>
               <Route path="/" element={<NewsBody />} />
